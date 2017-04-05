832a48cb6ca88660061e5049d3a02aee962ce9f9
roozbeh <GIT_COMMITTER_EMAIL>
2017-04-05 05:53:57 -0400

    too many things to mention

--- gitlab.sh
+++ gitlab.sh
@@ -2,7 +2,7 @@ repository_name=wiki
 git config --global credential.helper cache
 git init
 git add *
-git commit -a -F progress.txt
+git commit -a -m "too many things to mention"
 git remote add $repository_name https://RoozbehSanaei@gitlab.com/RoozbehSanaei/wiki.git
 git remote -v
 git push -u $repository_name master
